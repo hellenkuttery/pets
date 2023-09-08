7682cba34509a43158e3a5a010930bb58168fe4a
hellenkuttery <helen@clarusway.com>
2023-09-09 00:26:59 +0300

    add nba folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import CardContainer from './components/CardContainer';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+      <CardContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import data from "../helpers/data";
+import PlayerCard from "./PlayerCard";
+
+const CardContainer = () => {
+  return (
+    <Container>
+      <h1>Merhaba</h1>
+      {data.map((player) => {
+        return <PlayerCard {...player} />;
+      })}
+    </Container>
+  );
+};
+
+export default CardContainer;
